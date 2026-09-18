fd83a70640be8c379f3fd0ecd7314a449fa3923a
Sanjana <sanjananb803@gmail.com>
2026-09-18 14:26:05 +0530

    Checked the weather

---
+++ weather.py
@@ -0,0 +1,7 @@
+degree = int(input("Enter the degree: "))
+if degree <=20:
+    print("Cold weather!")
+elif degree > 20 and degree <=38:
+    print("Normal Weather!")
+else:
+    print("Hot Weather!")        
