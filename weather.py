7535134f7d9d26c4f25407a59a319144b98f398f
Sanjana <sanjananb803@gmail.com>
2026-09-18 14:52:29 +0530

    Added the conversion of fahrenheit

--- weather.py
+++ weather.py
@@ -4,4 +4,6 @@ if degree <=20:
 elif degree > 20 and degree <=38:
     print("Normal Weather!")
 else:
-    print("Hot Weather!")        
+    print("Hot Weather!")
+fahrenheit = ((degree * 1.8) + 32)
+print("The Fahrenheit value is: ",fahrenheit,"F")
